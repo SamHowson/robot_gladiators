2f483011d0794e8e36d4da9cfc67276a0d9514b7
SamHowson <davidhowson@Davids-MacBook-Air.local>
2021-12-18 10:33:24 -0700

    update2

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+//Window Prompt
+var playerName = window.prompt("What is your robots name?");
+//adding prompt for name
+console.log("Our Robot is!" + playerName);
+
+// Create a fight function
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+fight();
